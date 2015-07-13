87aa53c105b5be830d293b387f246d4eb3ce8492
Enrico Piazza <enry2077@gmail.com>
2015-07-13 23:50:00 +0200

    change of topic's naming in record command

--- bags/recbag.sh
+++ bags/recbag.sh
@@ -16,6 +16,6 @@ DATE=`date +%Y%m%d%k%M`
 
 xterm -e "roslaunch rockin_logger rockin_logger.launch" &
 
-rosbag record /tf /rockin/robot_pose /rockin/marker_pose /rockin/scan_0 /rockin/scan_1 /rockin/image \
-              /rockin/image /rockin/pointcloud /rockin/audio /rockin/command \
+rosbag record /tf /rockin/${TEAMNAME}/robot_pose /rockin/${TEAMNAME}/marker_pose /rockin/${TEAMNAME}/scan_0 /rockin/${TEAMNAME}/scan_1 /rockin/${TEAMNAME}/image \
+              /rockin/${TEAMNAME}/pointcloud /rockin/${TEAMNAME}/audio /rockin/${TEAMNAME}/command \
               -O "${BENCHMARK}_${DATE}_${TEAMNAME}.bag" 
